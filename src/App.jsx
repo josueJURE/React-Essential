64393a6a6e2c7aad0a9095fbac878b760b45712a
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2024-10-10 09:03:36 +0100

    (feat: JS): create generateRandomInteger()

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 
+const reactDescriptions = ["Fundamenta", "Crucial", "Core"];
+
+function generateRandomInteger(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
 function Header() {
+  const description = reactDescriptions[generateRandomInteger(reactDescriptions.length -1)]
   return (  <header>
     <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
-      Fundamental React concepts you will need for almost any app you are
+      {description} React concepts you will need for almost any app you are
       going to build!
     </p>
   </header>)
 }
+
+
+
+
 
 
 function App() {
