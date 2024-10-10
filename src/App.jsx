95a9201724c55313b49226ca2c7da8eee07b2aab
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2024-10-10 09:15:32 +0100

    (feat: JS): import image using the 'import keyword'

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 
-const reactDescriptions = ["Fundamenta", "Crucial", "Core"];
+import image from  "./assets/react-core-concepts.png" 
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core", "Vital"];
 
 function generateRandomInteger(max) {
   return Math.floor(Math.random() * (max + 1))
 }
 
 function Header() {
-  const description = reactDescriptions[generateRandomInteger(reactDescriptions.length -1)]
+  const description = reactDescriptions[generateRandomInteger(reactDescriptions.length -1)];
+
   return (  <header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <img src={image} alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
       {description} React concepts you will need for almost any app you are
